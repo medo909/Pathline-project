265a5919e3b1484e3b0501df3b644b7c787830b3
Mohamed Hossam <145234316+medo909@users.noreply.github.com>
2026-02-07 17:06:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello, welcome to my dream job page");
+
+  function changeText(){
+    document.getElementById("changeME").innerHTML="<br>Stephen Curry shoots the basketball like it's a superpower! 🏀✨ He runs fast, smiles big, and hits amazing shots from way, way far—sometimes it looks like magic. He plays with joy, dances after big shots, and makes basketball feel fun, exciting, and full of surprises, like a game anyone can dream of playing! 🌟";
+  }
+    function addText(){
+  document.getElementById("changing").innerHTML+=
+  "<a href='https://www.youtube.com/watch?v=h5wU0N61sfA' target='_blank'><br> <style>color:white;</style> Stephen curry highlights video</a><p><--- Click here</p>";
+  document.getElementById("button").disabled=true;
+}
+function changeColor(){
+  document.getElementById("changing").style.color="lightblue";
+}
+
